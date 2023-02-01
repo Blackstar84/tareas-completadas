e16e266a33f1daeb245980792e169d3116993949
Blackstar84 <carlosalmada84@gmail.com>
2023-02-01 17:30:15 -0300

    se agrega parte del código

--- src/App.js
+++ src/App.js
@@ -11,19 +11,46 @@ class App extends Component {
         { id: 2, name: "Hacer la cama", done: true },
         { id: 3, name: "Leer un rato", done: false }
       ],
-      newTask: ''
+      newTask: '',
+      valor: 3,
     }
   }
+  verificarTarea = (e) =>{
+    const completada = e.target;
+    if(completada.classList.contains('done')){
+      completada.classList.remove('done');
+    }else{
+      completada.classList.add('done');
+      
+    }
+  }
+
+  handlerOnType = (ev) => {
+    const valor = ev.target.value;  
+    console.log(this.task.length);  
+    const agregar = {id: this.task.length+1, name: valor, done: false}
+    this.setState({newTask:agregar});
+     
+
+  }
+  handlerOnSubmit=(event)=>{
+    event.preventDefault();
+    this.setState({task: this.state.tasks.concat([this.state.newTask])})
+    this.setState({newTask:""})
+
+    
+  };
+
   render() {
     return (
       <div className="wrapper">
         <div className="list">
           <h3>Por hacer:</h3>
           <ul className="todo">
-            {this.state.tasks.map((task, index) => <li key={task.id}>{task.name}</li>)}
+            {this.state.tasks.map((task, index) => <li key={task.id} onClick={this.verificarTarea}>{task.name}</li>)}
           </ul>
-          <form>
-            <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" value={this.state.newTask} />
+          <form onSubmit={this.handlerOnSubmit}>
+            <input type="text" id="new-task" onChange={this.handlerOnType} placeholder="Ingresa una tarea y oprime Enter" value={this.state.newTask} />
           </form>
         </div>
       </div>
